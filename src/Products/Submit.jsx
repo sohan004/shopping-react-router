b445202005f1880ec42cb4b1c3c519df17405a96
sohan004 <aryansohan02@gmail.com>
2023-04-03 21:36:36 +0600

    cng

--- src/Products/Submit.jsx
+++ src/Products/Submit.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Submit = () => {
     return (
         <div className='text-center'>
-            <img className='img-fluid' src="../../public/giphy.gif" alt="" />
+            <img className='img-fluid' src="giphy.gif" alt="" />
             <div className='text-center my-2'>
                 <button className="btn btn-secondary"><Link className='text-white' to='/'>Go To Home</Link></button>
             </div>
