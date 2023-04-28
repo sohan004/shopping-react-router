0c062c38387a1e077303d3fa2d7c9850b854c8a8
sohan004 <aryansohan02@gmail.com>
2023-04-29 02:16:59 +0600

    firebase file sequre

--- src/components/fireb.js
+++ src/components/fireb.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDN5VIRpBx8dHMo_BnQIL5C-w4UHKTCM2Q",
-    authDomain: "amar-zone.firebaseapp.com",
-    projectId: "amar-zone",
-    storageBucket: "amar-zone.appspot.com",
-    messagingSenderId: "469559561400",
-    appId: "1:469559561400:web:c65479af1d1e274871784d"
+    apiKey: import.meta.env.VITE_apiKey,
+    authDomain: import.meta.env.VITE_authDomain,
+    projectId: import.meta.env.VITE_projectId,
+    storageBucket: import.meta.env.VITE_storageBucket,
+    messagingSenderId: import.meta.env.VITE_messagingSenderId,
+    appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
